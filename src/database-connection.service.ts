f82307976fede75f6d52a282b6949e9bd090beb4
CodeWithSubaru <francis.cloydm@gmail.com>
2025-02-01 11:27:21 +0800

    initial commit

---
+++ src/database-connection.service.ts
@@ -0,0 +1,24 @@
+import {
+  TypeOrmModuleOptions,
+  TypeOrmOptionsFactory,
+} from './../node_modules/@nestjs/typeorm/dist/interfaces/typeorm-options.interface.d';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DatabaseConnectService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      name: 'default',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+      entities: ['dist/**/*.entity.js'],
+    };
+  }
+}
